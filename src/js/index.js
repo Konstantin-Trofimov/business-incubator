f899b1683ab0cac0970c8c4d138bd0e77b556572
Konstantin Trofimov <const.trofimov@gmail.com>
2022-07-28 17:23:42 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import * as $ from 'jquery'
+// import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+import '@/styles/style.scss';
+import './models/main';
+import WOW from './libs/wow/wow.min';
+
+
+new WOW().init();
+
+
+
+
+
+	
+
+
+
+
